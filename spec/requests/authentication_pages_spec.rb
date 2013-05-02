713d33866d4413a54430fd1314aa3ed071cd10df
soso0987 <go@happy.email.ne.jp>
2013-05-02 10:00:03 +0900

    Finish sign in 2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,13 +4,6 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
-
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
-  end
-
   describe "signin" do
     before { visit signin_path }
 
@@ -34,13 +27,8 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-
       describe "followed by signout" do
-        before { click_link "Sign out" }
+        before { click_link "Sign in" }
         it { should have_link('Sign in') }
       end
     end
